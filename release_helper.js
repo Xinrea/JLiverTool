51877a0374526307f548f2943b713ccea788da18
Xinrea <s11s@vip.qq.com>
2024-06-05 17:27:29 +0800

    chore: add helper for updating release page

---
+++ release_helper.js
@@ -0,0 +1,54 @@
+// get release list from github api
+// and then write it as a simple html file
+// https://api.github.com/repos/xinrea/JLiverTool/releases
+
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  hostname: 'api.github.com',
+  port: 443,
+  path: '/repos/xinrea/JLiverTool/releases',
+  method: 'GET',
+  headers: {
+    'User-Agent': 'Node.js'
+  }
+};
+
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (d) => {
+    data += d;
+  });
+  res.on('end', () => {
+    const releases = JSON.parse(data);
+    // list all releases with assets and desc
+    let html = '<html><head><title>JLiverTool Releases</title></head><body>';
+    html += '<h1>JLiverTool Releases</h1>';
+    // contact me
+    html += '<p>如有问题请联系 <a href="https://space.bilibili.com/475210">Xinrea</a></p>';
+    releases.forEach((release) => {
+      html += `<h2>${release.name}</h2>`;
+      // show published date in human readable format (GMT+8, 2024/01/01 08:00:00)
+      const published_at = new Date(release.published_at);
+      html += `<p>${published_at.toLocaleString('zh-CN', {timeZone: 'Asia/Shanghai'})}</p>`;
+      html += `<pre>${release.body}</pre>`;
+      html += '<ul>';
+      // list all assets except *.blockmap
+      release.assets.forEach((asset) => {
+        if (!asset.name.endsWith('.blockmap')) {
+          // download url need to be constructed as raw.vjoi.cn/jlivertool/ + name
+          html += `<li><a href="https://raw.vjoi.cn/jlivertool/${asset.name}">${asset.name}</a></li>`;
+        }
+      });
+      html += '</ul>';
+    });
+    fs.writeFileSync('releases.html', html);
+  });
+})
+
+req.on('error', (e) => {
+  console.error(e);
+})
+
+req.end();
